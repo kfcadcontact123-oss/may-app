0fd1281b420490e0443c79d9e556451b0d947466
KhanhDevHUST <kfcadcontact123@gmail.com>
2026-04-15 18:13:08 +0700

    fix stuff

--- web_project/wsgi.py
+++ web_project/wsgi.py
@@ -13,5 +13,5 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web_project.settings')
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+os.environ["GOOGLE_APPLICATION_CREDENTIALS_JSON"] = os.getenv("GOOGLE_APPLICATION_CREDENTIALS_JSON")
 application = get_wsgi_application()
