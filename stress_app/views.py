eb33ccdaa73fa40f07c4e64c78f8b2861802adf6
KhanhDevHUST <kfcadcontact123@gmail.com>
2026-04-17 13:11:26 +0700

    fix things

--- stress_app/views.py
+++ stress_app/views.py
@@ -125,7 +125,6 @@ def home(request):
     # USER + CHAT
     # =====================================
 
-    suggested_users = User.objects.exclude(id=user.id).select_related('usermemory')[:5]
     messages = ChatMessage.objects.filter(
         user=user
     ).order_by("created_at")
